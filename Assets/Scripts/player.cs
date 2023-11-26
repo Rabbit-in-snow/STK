7612e5640cfeaed196a5602964d66e35e3b4f6b7
Daumss <likedaum23@gmail.com>
2023-11-26 20:55:33 +0900

    animation

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,151 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class player : MonoBehaviour
+{
+    public short lv = 1;
+    public int Maxhealth;
+    public int CurrentHealth;
+    Vector2 Moveing;
+    public float Speed = 2f;
+    private Rigidbody2D rb;
+    private Animator am;
+    public uint xp;
+    public uint lvupxp;
+    private float nohurttime = (float)0.02;
+    public int attackpower = 1;
+    public int jumppower = 2;
+    [HideInInspector] public bool attacking;
+    [HideInInspector] public bool IsHurt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = Maxhealth;
+        rb = GetComponent<Rigidbody2D>();
+        am = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            attacking = true;
+        }
+        else if (!Input.GetKey(KeyCode.P))
+        {
+            attacking = false;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector2(0, 180);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            transform.eulerAngles = new Vector2(0, 0);
+        }
+        if (xp >= lvupxp)
+        {
+            am.Play("lvup");
+            lv++;
+            xp = 0;
+            nohurttime += 0.015f;
+            attackpower++;
+            Maxhealth++;
+            CurrentHealth++;
+            Speed+=0.5f;
+            jumppower++;
+        }
+        else if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) & IsHurt == true && attacking == false)
+        {
+            am.Play("hurt" + lv);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == false)
+        {
+            Moveing = new Vector2(1, 0);
+            am.Play("walk" + lv);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.UpArrow) && IsHurt == true)
+        {
+            Moveing = new Vector2(1, 0);
+            am.Play("walkhurt" + lv);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == false)
+        {
+            Moveing = new Vector2(2, 0);
+            am.Play("run" + lv);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow) && IsHurt == true)
+        {
+            Moveing = new Vector2(2, 0);
+            am.Play("runhurt" + lv);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == false)
+        {
+            Moveing = new Vector2(-1, 0);
+            am.Play("walk" + lv);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.UpArrow) && IsHurt == true)
+        {
+            Moveing = new Vector2(-1, 0);
+            am.Play("walkhurt" + lv);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == false)
+        {
+            Moveing = new Vector2(-2, 0);
+            am.Play("run" + lv);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow) && IsHurt == true)
+        {
+            Moveing = new Vector2(-2, 0);
+            am.Play("runhurt" + lv);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == true)
+        {
+            Moveing = new Vector2(0, 0);
+            am.Play("walkattack" + lv);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == true)
+        {
+            Moveing = new Vector2(0, 0);
+            am.Play("runattack" + lv);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == true)
+        {
+            Moveing = new Vector2(0, 0);
+            am.Play("walkattack" + lv);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow) && IsHurt == false && attacking == true)
+        {
+            Moveing = new Vector2(0, 0);
+            am.Play("runattack" + lv);
+        }
+        else if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) & IsHurt == false && attacking == true)
+        {
+            Moveing = new Vector2(0, 0);
+            am.Play("attack" + lv);
+        }
+        else
+        {
+            Moveing = new Vector2(0, 0);
+            am.Play("idle" + lv);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) //jump
+        {
+            Moveing += new Vector2(0, Speed+100f);
+        }
+
+        rb.MovePosition(new Vector2(transform.position.x, transform.position.y) + Speed * Time.deltaTime * Moveing.normalized);
+    }
+
+    public IEnumerator hurt(int hurtpower)
+    {
+        IsHurt = true;
+        CurrentHealth -= hurtpower;
+        yield return new WaitForSeconds(nohurttime);
+        IsHurt = false;
+    }
+}
