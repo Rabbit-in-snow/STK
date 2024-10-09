c1a5b18cca69e3d5561d981993e0c13d07471e1c
Daumss <STK@mczone.kro.kr>
2024-10-09 15:11:23 +0900

    Make duck

--- Assets/Script/Camera.cs
+++ Assets/Script/Camera.cs
@@ -1,164 +1,51 @@
-#if ENABLE_INPUT_SYSTEM && ENABLE_INPUT_SYSTEM_PACKAGE
-#define USE_INPUT_SYSTEM
-using UnityEngine.InputSystem;
-#endif
-
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace UnityEngine.Rendering
+​
+
+public class Camera : MonoBehaviour
+
 {
-    public class FreeCamera : MonoBehaviour
+    private GameObject go_Target;    // 타겟 설정을 해 줄 객체 선언
+
+​
+
+    [SerializeField]
+    private float speed;    //카메라가 go_Target을 쫓을 스피드 역시 수정해줄 수 있도록.
+
+​
+
+    private Vector3 difValue;  
+
+ //go_Target과 카메라 사이의 거리를 항상 같게 유지해 줄 수 있도록 difValue라는 변수 사용
+​
+
+    void Start()
+
     {
-        const float k_MouseSensitivityMultiplier = 0.01f;
+        go_Target=
+        difValue = transform.position - go_Target.transform.position;
 
-        /// <summary>
-        /// Rotation speed when using a controller.
-        /// </summary>
-        public float m_LookSpeedController = 120f;
-        /// <summary>
-        /// Rotation speed when using the mouse.
-        /// </summary>
-        public float m_LookSpeedMouse = 4.0f;
+        //difValue는 현재카메라의 위치 - 타겟의 위치여야 한다. 하지만 이 때, 음수의 값이 나올 수도 있기 때문에 아래의 절대 값 변환 식으로 항상 양수가 나올 수 있도록 해 준다.
 
-#if !USE_INPUT_SYSTEM
-        private static string kMouseX = "Mouse X";
-        private static string kMouseY = "Mouse Y";
-        private static string kRightStickX = "Controller Right Stick X";
-        private static string kRightStickY = "Controller Right Stick Y";
-        private static string kVertical = "Vertical";
-        private static string kHorizontal = "Horizontal";
+        difValue = new Vector3(Mathf.Abs(difValue.x), Mathf.Abs(difValue.y), Mathf.Abs(difValue.z));
 
-        private static string kYAxis = "YAxis";
-        private static string kSpeedAxis = "Speed Axis";
-#endif
+        // Vector 3 (x, y, z) 모두 양수가 될 수 있도록 Mathf.Abs (절대 값)을 넣어 모두 양수로 제한해 준다. 
 
-#if USE_INPUT_SYSTEM
-        InputAction lookAction;
-        InputAction moveAction;
-        InputAction speedAction;
-        InputAction yMoveAction;
-#endif
 
-        void OnEnable()
-        {
-            RegisterInputs();
-        }
 
-        void RegisterInputs()
-        {
-#if USE_INPUT_SYSTEM
-            var map = new InputActionMap("Free Camera");
+    }​
 
-            lookAction = map.AddAction("look", binding: "<Mouse>/delta");
-            moveAction = map.AddAction("move", binding: "<Gamepad>/leftStick");
-            speedAction = map.AddAction("speed", binding: "<Gamepad>/dpad");
-            yMoveAction = map.AddAction("yMove");
+    void Update()
 
-            lookAction.AddBinding("<Gamepad>/rightStick").WithProcessor("scaleVector2(x=15, y=15)");
-            moveAction.AddCompositeBinding("Dpad")
-                .With("Up", "<Keyboard>/w")
-                .With("Up", "<Keyboard>/upArrow")
-                .With("Down", "<Keyboard>/s")
-                .With("Down", "<Keyboard>/downArrow")
-                .With("Left", "<Keyboard>/a")
-                .With("Left", "<Keyboard>/leftArrow")
-                .With("Right", "<Keyboard>/d")
-                .With("Right", "<Keyboard>/rightArrow");
-            speedAction.AddCompositeBinding("Dpad")
-                .With("Up", "<Keyboard>/home")
-                .With("Down", "<Keyboard>/end");
-            yMoveAction.AddCompositeBinding("Dpad")
-                .With("Up", "<Keyboard>/pageUp")
-                .With("Down", "<Keyboard>/pageDown")
-                .With("Up", "<Keyboard>/e")
-                .With("Down", "<Keyboard>/q")
-                .With("Up", "<Gamepad>/rightshoulder")
-                .With("Down", "<Gamepad>/leftshoulder");
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difValue, speed);
 
-            moveAction.Enable();
-            lookAction.Enable();
-            speedAction.Enable();
-            yMoveAction.Enable();
-#endif
-
-#if UNITY_EDITOR && !USE_INPUT_SYSTEM
-            List<InputManagerEntry> inputEntries = new List<InputManagerEntry>();
-
-            // Add new bindings
-            inputEntries.Add(new InputManagerEntry { name = kRightStickX, kind = InputManagerEntry.Kind.Axis, axis = InputManagerEntry.Axis.Fourth, sensitivity = 1.0f, gravity = 1.0f, deadZone = 0.2f });
-            inputEntries.Add(new InputManagerEntry { name = kRightStickY, kind = InputManagerEntry.Kind.Axis, axis = InputManagerEntry.Axis.Fifth, sensitivity = 1.0f, gravity = 1.0f, deadZone = 0.2f, invert = true });
-
-            inputEntries.Add(new InputManagerEntry { name = kYAxis, kind = InputManagerEntry.Kind.KeyOrButton, btnPositive = "page up", altBtnPositive = "joystick button 5", btnNegative = "page down", altBtnNegative = "joystick button 4", gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
-            inputEntries.Add(new InputManagerEntry { name = kYAxis, kind = InputManagerEntry.Kind.KeyOrButton, btnPositive = "q", btnNegative = "e", gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
-
-            inputEntries.Add(new InputManagerEntry { name = kSpeedAxis, kind = InputManagerEntry.Kind.KeyOrButton, btnPositive = "home", btnNegative = "end", gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
-            inputEntries.Add(new InputManagerEntry { name = kSpeedAxis, kind = InputManagerEntry.Kind.Axis, axis = InputManagerEntry.Axis.Seventh, gravity = 1000.0f, deadZone = 0.001f, sensitivity = 1000.0f });
-
-            InputRegistering.RegisterInputs(inputEntries);
-#endif
-        }
-
-        float inputRotateAxisX, inputRotateAxisY;
-        float inputVertical, inputHorizontal, inputYAxis;
-
-        void UpdateInputs()
-        {
-            inputRotateAxisX = 0.0f;
-            inputRotateAxisY = 0.0f;
-
-#if USE_INPUT_SYSTEM
-            var lookDelta = lookAction.ReadValue<Vector2>();
-            inputRotateAxisX = lookDelta.x * m_LookSpeedMouse * k_MouseSensitivityMultiplier;
-            inputRotateAxisY = lookDelta.y * m_LookSpeedMouse * k_MouseSensitivityMultiplier;
-
-            leftShift = Keyboard.current?.leftShiftKey?.isPressed ?? false;
-            fire1 = Mouse.current?.leftButton?.isPressed == true || Gamepad.current?.xButton?.isPressed == true;
-
-            inputChangeSpeed = speedAction.ReadValue<Vector2>().y;
-
-            var moveDelta = moveAction.ReadValue<Vector2>();
-            inputVertical = moveDelta.y;
-            inputHorizontal = moveDelta.x;
-            inputYAxis = yMoveAction.ReadValue<Vector2>().y;
-#else
-            if (Input.GetMouseButton(1))
-            {
-                inputRotateAxisX = Input.GetAxis(kMouseX) * m_LookSpeedMouse;
-                inputRotateAxisY = Input.GetAxis(kMouseY) * m_LookSpeedMouse;
-            }
-            inputRotateAxisX += (Input.GetAxis(kRightStickX) * m_LookSpeedController * k_MouseSensitivityMultiplier);
-            inputRotateAxisY += (Input.GetAxis(kRightStickY) * m_LookSpeedController * k_MouseSensitivityMultiplier);
-
-            inputVertical = Input.GetAxis(kVertical);
-            inputHorizontal = Input.GetAxis(kHorizontal);
-            inputYAxis = Input.GetAxis(kYAxis);
-#endif
-        }
-
-        void Update()
-        {
-            // If the debug menu is running, we don't want to conflict with its inputs.
-            if (DebugManager.instance.displayRuntimeUI)
-                return;
-
-            UpdateInputs();
-
-            bool moved = inputRotateAxisX != 0.0f || inputRotateAxisY != 0.0f || inputVertical != 0.0f || inputHorizontal != 0.0f || inputYAxis != 0.0f;
-            if (moved)
-            {
-                float rotationX = transform.localEulerAngles.x;
-                float newRotationY = transform.localEulerAngles.y + inputRotateAxisX;
-
-                // Weird clamping code due to weird Euler angle mapping...
-                float newRotationX = (rotationX - inputRotateAxisY);
-                if (rotationX <= 90.0f && newRotationX >= 0.0f)
-                    newRotationX = Mathf.Clamp(newRotationX, 0.0f, 90.0f);
-                if (rotationX >= 270.0f)
-                    newRotationX = Mathf.Clamp(newRotationX, 270.0f, 360.0f);
-
-                transform.localRotation = Quaternion.Euler(newRotationX, newRotationY, transform.localEulerAngles.z);
-            }
-        }
+        // 카메라의 위치는 = Vector3. Lerp (Lerp는 움직임에 부드러움을 주기 위함이다.-- ( a , b, t )의 값을 갖는데 a는 이동전의 위치, b는 이동할 위치, t는 움직임의 비율을 뜻한다.)
+        // t (여기서는 speed) 의 값은 0~1의 사이가 된다. a와 b사이의 비율을 뜻하기 때문에 0.5를 기입한다면 a와 b의 중간 값을 return하여 준다. 
+        // Unity 화면으로 돌아가 스크립트를 넣어준 후 speed를 0.05(적은 수)로 넣어주면 아주 부드럽게 따라 가는 모습을,
+        //1(최대 수)을 넣어주면 따라가는 것인지 알 수 없을 정도의 모습을 볼 수 있다. (타겟의 움직임이 바로 카메라의 위치가 되기 때문에 화면이 정지되어 있는 것 처럼 움직인다.)​
     }
+
 }
